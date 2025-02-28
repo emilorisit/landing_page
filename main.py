8e82502a1d4f8cee85522a65126432103e130226
emilniclas <40062799-emilniclas@users.noreply.replit.com>
2025-02-28 14:05:31 +0000

    Enhance static file serving to handle all paths.
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/325be46f-8c35-4ec9-84fb-6e0e20c524f6/20eb56e9-fd09-4203-accb-39fa5f2c86df.jpg

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='www')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return send_from_directory('www', 'index.html')
+
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory('www', path)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
